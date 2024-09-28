f0e24f5902a8c82b44b2edb9cc3ae9381367c05a
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-09-28 13:25:47 +0500

    Added Remove btn in javascript

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ var input = document.getElementById("input");
 var text = document.querySelector(".text");
 
 function addTask(){
-    if (!input.ariaValueMax.trim()){
+    if (!input.value.trim()){
         alert("Add Something!");
-    }else{
+    } else {
         var li = document.createElement("li");
         li.textContent = input.value;
 
+        var  removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.className = 'remove-btn';
+        removeButton.onclick = function () {
+            text.removeChild(li);
+        };
+        
+        li.appendChild(removeButton);
         li.appendChild(li);
         input.value = "";
     }
